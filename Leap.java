7a868444d57e829e9caa99fea15f53e2a2839613
raihanabanu <raihanabanu.s@gmail.com>
2016-07-20 11:48:46 +0530

    Rename leap1 to Leap.java

---
+++ Leap.java
@@ -0,0 +1,17 @@
+# leap
+import java.util.Scanner;
+class Leap{
+public static void main(String args[]){
+Scanner cc=new Scanner(System.in);
+System.out.println(" enter the year");
+int n =cc.nextInt();
+if(n%400==0){
+ if((n%4==0)&&(n%100==0)){
+ System.out.println("the year is leap year");
+ }
+}
+ else{
+ System.out.println("the given year is not leap year");
+ }
+ }
+ }	
